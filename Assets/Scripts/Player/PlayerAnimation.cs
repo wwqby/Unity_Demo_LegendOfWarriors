620719a071769a07a5d4f9b78c371790beb18714
王万泉 <Wongwanquanmail@gmail.com>
2024-04-17 11:36:42 +0800

    添加跳跃的混合树动画

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -9,10 +9,12 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
 
     private void Awake() {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
     }
 
     private void Update() {
@@ -20,11 +22,14 @@ public class PlayerAnimation : MonoBehaviour
     }
 
     /// <summary>
-    /// 设置动画吃的控制参数
+    /// 设置动画控制参数
     /// </summary>
     private void SetAnmimation()
     {   
-        //设置人物移动速度velocityX
+        //设置人物移动速度
         animator.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("velocityY",rb.velocity.y);
+        //设置人物是否在地面
+        animator.SetBool("isGround",physicsCheck.isGround);
     }
 }
